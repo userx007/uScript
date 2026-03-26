a479f02d9845eefcaff53853d7a0b57e8af4d5d6
userx007 <vmpxxl@gmail.com>
2026-03-26 08:38:54 +0100

    Add udevrules script to install the necessary udev rules corresponding to some uded devices
    Signed-off-by: userx007 <vmpxxl@gmail.com>

---
+++ udev/install_udev_rules.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+VENDOR="10C4"
+PRODUCT="EA90"
+RULES_FILE="/etc/udev/rules.d/99-hidraw.rules"
+
+cat > "$RULES_FILE" << EOF
+SUBSYSTEM=="hidraw", ATTRS{idVendor}=="$VENDOR", ATTRS{idProduct}=="$PRODUCT", MODE="0666"
+EOF
+
+# Reload udev rules
+udevadm control --reload-rules
+udevadm trigger
+
+echo "udev rule installed: $RULES_FILE"
