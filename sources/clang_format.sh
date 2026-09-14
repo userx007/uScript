b847f9c8488d6b74e343aa8855ee95f9095e4ca5
userx007 <vmpxxl@gmail.com>
2026-09-14 10:46:40 +0200

    Fix formater
    Signed-off-by: userx007 <vmpxxl@gmail.com>

--- sources/clang_format.sh
+++ sources/clang_format.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-# sudo apt install clang-format
+CLANG_FORMAT="$HOME/.local/bin/clang-format"
 
-find . \( -name "*.cpp" -o -name "*.h" -o -name "*.hpp" \) -exec clang-format -i {} +
-
-# alternative
-# find . -regex '.*\.\(cpp\|cc\|cxx\|h\|hpp\)' -exec clang-format -i {} +
-
-# Format only Git-tracked files
-# git ls-files '*.cpp' '*.cc' '*.cxx' '*.h' '*.hpp' | xargs clang-format -i
-
-# disable formating inside some files
-# // clang-format off
-# ...
-#// clang-format on
+find . \( -name "*.cpp" -o -name "*.h" -o -name "*.hpp" \) -exec "$CLANG_FORMAT" -i {} +
